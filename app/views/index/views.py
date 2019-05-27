fc2f825d886a16b84538dfaaca1370a492b30ebc
lisiyuan <1803510559@qq.com>
2019-05-27 21:07:10 +0800

    修改视图函数
    Signed-off-by: lisiyuan <1803510559@qq.com>

--- app/views/index/views.py
+++ app/views/index/views.py
@@ -8,7 +8,7 @@ from app import db
 @index_blu.route("/index")
 @index_blu.route("/")
 def index():
-    # 1. 查询点击排行的前6个数据
-    news = db.session.query(News).order_by(News.clicks.desc()).limit(6)  # 从news数据表中查询点击最高的6个数据
-    
+    # 1.查询点击排行的前六个数据
+    news = db.session.query(News).order_by(News.clicks.desc()).limit(6)
+
     return render_template("index/index.html", news=news)
