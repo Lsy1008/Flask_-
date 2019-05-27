6500e99198350aed180a44564fe17262bcc6aaee
lisiyuan <1803510559@qq.com>
2019-05-27 20:51:43 +0800

    整理代码
    Signed-off-by: lisiyuan <1803510559@qq.com>

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from flask_migrate import Migrate, MigrateCommand
 
 from app import create_app, db
 
-
 if __name__ == '__main__':
-    # 1. 创建flask对象
+    # 1.创建flask对象
     app = create_app("development")
     # app.run()
     # 2. 创建Manger对象
@@ -13,6 +12,6 @@ if __name__ == '__main__':
     # 3. 创建数据库迁移对象
     migrate = Migrate(app, db)
     # 4. 将数据库迁移时的命令添加到manager上
-    manager.add_command('db', MigrateCommand)
+    manager.add_command("db", MigrateCommand)
     # app.run()
     manager.run()
